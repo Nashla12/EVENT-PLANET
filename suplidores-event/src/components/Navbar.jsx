a09c9ef21110dc6c64a6e3781c86773bf852f2be
nashla12 <nashlaguzman1111@gmail.com>
2025-05-08 21:18:50 -0400

    creacion de archivos

---
+++ suplidores-event/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="bg-pink-500 text-white p-4 flex justify-between items-center">
+      <h1 className="font-bold text-xl">Evento Perfecto</h1>
+      <div className="space-x-4">
+        <Link to="/" className="hover:underline">Inicio</Link>
+        <Link to="/servicios" className="hover:underline">Servicios</Link>
+        <Link to="/productos" className="hover:underline">Productos</Link>
+        <Link to="/vende" className="hover:underline">Vende con nosotros</Link>
+        <Link to="/login" className="hover:underline">Login</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
